bf7dc07fecc1ec677b9e3bbf59cba4d73e33ac14
jatinarora-materialplus <jatin.arora@materialplus.io>
2025-02-10 15:34:45 +0530

    resolves error

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,10 +1,9 @@
 from src.math_operation import add,sub
 
-def test_add(a,b):
+def test_add():
     assert add(3,5)==8
     assert add(-5,3)==-2
 
-def test_sub(a,b):
+def test_sub():
     assert sub(3,1)==2
     assert sub(-5,2)==-7
-      
