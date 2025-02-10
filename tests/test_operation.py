19f2f8c5de285dbe55e0ba9c80373b8531fcf9c2
jatinarora-materialplus <jatin.arora@materialplus.io>
2025-02-10 15:14:39 +0530

    added test cases

---
+++ tests/test_operation.py
@@ -0,0 +1,10 @@
+from src.math_operation import add,sub
+
+def test_add(a,b):
+    assert add(3,5)==8
+    assert add(-5,3)==-2
+
+def test_sub(a,b):
+    assert sub(3,1)==2
+    assert sub(-5,2)==-7
+      
